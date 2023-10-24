e80f096e421cc725635f85e0e3e6e082a8ae4596
jah3 <isecov9@gmail.com>
2023-10-24 11:54:52 +0300

    main

--- js/script.js
+++ js/script.js
@@ -1,35 +1,63 @@
-  // Get a reference to the search input field
-  const searchInput = document.getElementById("search-input");
-  const searchButton = document.getElementById("search-button");
+// Get a reference to the search input field
+const searchInput = document.getElementById("search-input");
 
-  searchButton.addEventListener("click", function () {
-    // Get the value of the search input
-    const searchValue = searchInput.value.toLowerCase();
+// Add an event listener to the search input
+searchInput.addEventListener("input", function () {
+  // Get the value of the search input
+  const searchValue = this.value.toLowerCase();
 
-    // Get references to all card elements
-    const cardElements = document.querySelectorAll(".card");
+  // Get references to all card elements
+  const cardElements = document.querySelectorAll(".card");
 
-    let firstMatch = null; // To store the first matched card
+  // Loop through all card elements
+  cardElements.forEach(function (card) {
+    // Get the title text within each card (you can modify this based on your actual HTML structure)
+    const title = card.querySelector("h2").textContent.toLowerCase();
 
-    // Loop through all card elements
-    cardElements.forEach(function (card) {
-      // Get the title text within each card (modify this based on your actual HTML structure)
-      const title = card.querySelector("h2").textContent.toLowerCase();
-
-      // Check if the title contains the search value
-      if (title.includes(searchValue)) {
-        // Show the card if it matches the search
-        card.classList.remove("hidden");
-        if (!firstMatch) firstMatch = card; // Set the first matched card
-      } else {
-        // Hide the card if it doesn't match the search
-        card.classList.add("hidden");
-      }
-    });
-
-    // Scroll to the first matched card
-    if (firstMatch) firstMatch.scrollIntoView({ behavior: "smooth", block: "start" });
+    // Check if the title contains the search value
+    if (title.includes(searchValue)) {
+      // Show the card if it matches the search
+      card.style.display = "block";
+    } else {
+      // Hide the card if it doesn't match the search
+      card.style.display = "none";
+    }
   });
+  // Get a reference to the search input field
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+searchButton.addEventListener("click", function () {
+  // Get the value of the search input
+  const searchValue = searchInput.value.toLowerCase();
+
+  // Get references to all card elements
+  const cardElements = document.querySelectorAll(".card");
+
+  let matchedCard = null; // To store the matched card
+
+  // Loop through all card elements
+  cardElements.forEach(function (card) {
+    // Get the title text within each card (modify this based on your actual HTML structure)
+    const title = card.querySelector("h2").textContent.toLowerCase();
+
+    // Check if the title contains the search value
+    if (title.includes(searchValue)) {
+      // Show the card if it matches the search
+      card.classList.remove("hidden");
+      matchedCard = card; // Set the matched card
+    } else {
+      // Hide all other cards
+      card.classList.add("hidden");
+    }
+  });
+
+  // Scroll to the matched card if one exists
+  if (matchedCard) matchedCard.scrollIntoView({ behavior: "smooth", block: "start" });
+});
+});
+
+
 
 // Get references to the button and the pop-up
 const loginBtn = document.getElementById("loginBtn");
